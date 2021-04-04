a59f60384de365338b8a25ed2fec7a04103f3b9a
skuzzymiglet <skuzzymiglet@gmail.com>
2021-04-04 23:31:15 +0100

    noo birjon

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module i
 
 go 1.15
 
-require github.com/gizak/termui/v3 v3.1.0
+require (
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/kr/pretty v0.2.1
+	github.com/muesli/termenv v0.8.1
+	github.com/neovim/go-client v1.1.7
+)
--- main.go
+++ main.go
@@ -1,4 +1,65 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/muesli/termenv"
+	"github.com/neovim/go-client/nvim"
+)
+
 func main() {
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatalf("Wrong number of args: %d, need 1 (Neovim RPC dial address)", flag.NArg())
+	}
+	n, err := nvim.Dial(flag.Arg(0))
+	if err != nil {
+		panic(err)
+	}
+	cw, err := n.CurrentWindow()
+	if err != nil {
+		panic(err)
+	}
+	b, err := n.WindowBuffer(cw)
+	if err != nil {
+		panic(err)
+	}
+	var (
+		lastTick   int
+		cancelFunc context.CancelFunc
+		ctx        context.Context
+	)
+	for {
+		tick, err := n.BufferChangedTick(b)
+		if err != nil {
+			panic(err)
+		}
+		if tick > lastTick {
+			termenv.ClearScreen()
+			if cancelFunc != nil {
+				cancelFunc()
+			}
+			r := nvim.NewBufferReader(n, b)
+			br := bufio.NewReader(r)
+			cmd, err := br.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+
+			ctx, cancelFunc = context.WithCancel(context.Background())
+			c := exec.CommandContext(ctx, "sh", "-c", cmd)
+			c.Stdin = br
+			c.Stderr = os.Stderr
+			c.Stdout = os.Stderr
+			c.Start()
+		}
+		lastTick = tick
+		time.Sleep(time.Millisecond * 100)
+	}
 }
